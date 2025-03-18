9d9ddd47a96f10c6fb124c3b7000a70971f8e6ca
Yago Michel <santiagomichelh@gmail.com>
2025-03-18 02:38:25 -0600

    ajustando controladores

--- src/main/java/com/yago/Examen/controllers/homeController.java
+++ src/main/java/com/yago/Examen/controllers/homeController.java
@@ -2,6 +2,7 @@ package com.yago.Examen.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -10,6 +11,10 @@ public class homeController {
     @GetMapping()
     public String home_index() {
         return "index";
+    }
+    @GetMapping("index/admin")
+    public String home_index_admin() {
+        return "index-admin";
     }
     
 }
--- src/main/java/com/yago/Examen/controllers/loginController.java
+++ src/main/java/com/yago/Examen/controllers/loginController.java
@@ -2,6 +2,7 @@ package com.yago.Examen.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -18,6 +19,18 @@ public class loginController {
     @GetMapping("/forgot")
     public String get_forgot() {
         return "login_singUp/forgot";
+    }
+    @PostMapping("/login")
+    public String post_login() {
+        return "index-admin";
+    }
+    @PostMapping("/forgot")
+    public String post_forgot() {
+        return "login_singUp/login";
+    }
+    @PostMapping("/singup")
+    public String post_singup() {
+        return "index-admin";
     }
     
 }
